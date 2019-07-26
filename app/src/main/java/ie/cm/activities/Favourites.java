05bb18464030e170383adcc3389959025a300ab1
manishdua <mdua007@gmail.com>
2019-07-25 23:10:31 -0400

    version 3.0

---
+++ app/src/main/java/ie/cm/activities/Favourites.java
@@ -0,0 +1,25 @@
+package ie.cm.activities;
+
+import ie.cm.R;
+import ie.cm.fragments.CoffeeFragment;
+import ie.cm.fragments.SearchFragment;
+
+import android.os.Bundle;
+
+public class Favourites extends Base {
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.favourites);
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+
+		coffeeFragment = CoffeeFragment.newInstance(); // get a new Fragment instance
+		getFragmentManager()
+				.beginTransaction()
+				.replace(R.id.fragment_layout, coffeeFragment)
+				.commit(); // add/replace in the current activity
+	}
+}
---
+++ app/src/main/java/ie/cm/activities/Search.java
@@ -0,0 +1,23 @@
+package ie.cm.activities;
+
+import ie.cm.R;
+import ie.cm.fragments.SearchFragment;
+
+import android.os.Bundle;
+
+public class Search extends Base {
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.search);
+	}
+	@Override
+	protected void onResume() {
+		super.onResume();
+
+		coffeeFragment = SearchFragment.newInstance(); // get a new Fragment instance
+		getFragmentManager()
+				.beginTransaction()
+				.replace(R.id.fragment_layout, coffeeFragment)
+				.commit(); // add/replace in the current activity
+	}
+}
